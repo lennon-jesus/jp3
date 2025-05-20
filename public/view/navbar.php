671d47221e0054477b7850941902ab680980a90b
lennon-jesus <161793432+lennon-jesus@users.noreply.github.com>
2025-05-20 19:38:57 -0300

    Add files via upload

--- public/view/navbar.php
+++ public/view/navbar.php
@@ -1,23 +1,36 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light border-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="index.php"><img id="nav" alt="Hotel Winner" src="../img/winner.png" height="70"></a>
+        <a class="navbar-brand" href="index.php"><img id="nav" alt="Hotel Winner" src="../img/winner.png"
+                height="70"></a>
         <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="gerencliente.php">Gerenciamento</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="#">Relatórios</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="atendimento.php">Atendimento</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" href="logout.php" tabindex="-1" aria-disabled="true">
-            <span class="material-symbols-outlined">logout</span>
-            </a>
-            </li>
-        </ul>
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Gerenciamento
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="gerencliente.php">Cliente</a></li>
+                        <li><a class="dropdown-item" href="gerenagend.php">Agendamento</a></li>
+                        <li><a class="dropdown-item" href="gerenquarto.php">Quarto</a></li>
+                        <li><a class="dropdown-item" href="gerenfunc.php">Funcionário</a></li>
+                        <li><a class="dropdown-item" href="gerenpedido.php">Pedido</a></li>
+                        <li><a class="dropdown-item" href="gerenproduto.php">Produto</a></li>
+                        <li><a class="dropdown-item" href="gerenpatrimonio.php">Patrimônio</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Relatórios</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="atendimento.php">Atendimento</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="logout.php" tabindex="-1" aria-disabled="true">
+                        <span class="material-symbols-outlined">logout</span>
+                    </a>
+                </li>
+            </ul>
         </div>
     </div>
 </nav>
